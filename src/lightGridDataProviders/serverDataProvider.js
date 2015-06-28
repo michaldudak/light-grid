881c93dbf10f9923dc263cb7bccfa04c8e225037
Michał Dudak <michal.dudak@gmail.com>
2015-06-28 11:58:07 +0200

    Server data provider - reading remote data

---
+++ src/lightGridDataProviders/serverDataProvider.js
@@ -0,0 +1,156 @@
+function ServerDataProvider(resourceUrl, $http, $timeout, defaultViewSettings, debounceTime) {
+
+	var viewSettings = angular.copy(defaultViewSettings);
+	var viewModel;
+	var filteredItemCount;
+	var pendingRequest = null;
+	this.debounceTime = debounceTime;
+
+	function updateFilters(requestSettings) {
+		if (!resourceUrl) {
+			return;
+		}
+		
+		if (!requestSettings) {
+			requestSettings = viewSettings;
+		} else {
+			requestSettings = angular.extend({}, viewSettings, requestSettings);
+		}
+		
+		var url = resourceUrl;
+		
+		var queryString = [];
+		
+		if (requestSettings.limitTo) {
+			if (requestSettings.limitTo.limit) {
+				queryString.push("limit=" + requestSettings.limitTo.limit);
+			}
+			
+			if (requestSettings.limitTo.begin) {
+				queryString.push("begin=" + requestSettings.limitTo.begin);
+			}
+		}
+		
+		if (requestSettings.orderBy && requestSettings.orderBy.expression) {
+			queryString.push("orderBy=" + encodeURIComponent(requestSettings.orderBy.expression));
+			
+			if (requestSettings.orderBy.reverse) {
+				queryString.push("reverse=true");
+			}
+		}
+		
+		if (requestSettings.filter && requestSettings.filter.expression) {
+			queryString.push("search=" + encodeURIComponent(requestSettings.filter.expression));
+		}
+		
+		if (queryString.length > 0) {
+			if (url.indexOf("?") === -1) {
+				url += "?";
+			} else {
+				url += "&";
+			}
+			
+			url += queryString.join("&");
+		}
+		
+		if (pendingRequest !== null) {
+			$timeout.cancel(pendingRequest);
+			pendingRequest = null;
+		}
+		
+		pendingRequest = $timeout(function() {
+			pendingRequest = null;
+			$http.get(url).success(function(response) {
+				viewModel = response.data;
+				filteredItemCount = response.totalResults;
+				viewSettings = requestSettings;
+			});
+		}, this.debounceTime);
+	}
+
+	this.getGridModel = function() {
+		return viewModel;
+	};
+
+	this.getModelItemCount = function () {
+		return filteredItemCount;
+	};
+
+	this.getCurrentViewSettings = function() {
+		return viewSettings;
+	};
+
+	this.saveModel = function (model) {
+		return $http.post(resourceUrl, model);
+	};
+
+	this.orderBy = function (expression, reverse) {
+		var requestSettings = {
+			orderBy: {
+				expression: expression,
+				reverse: reverse || false
+			}
+		};
+
+		updateFilters(requestSettings);
+	};
+
+	this.limitTo = function (limit, begin) {
+		var requestSettings = {};
+		
+		if (limit === undefined || limit === null) {
+			requestSettings.limitTo = null;
+		} else {
+			requestSettings.limitTo = {
+				limit: limit,
+				begin: begin || 0
+			};
+		}
+
+		updateFilters(requestSettings);
+	};
+
+	this.filter = function (expression) {
+		var requestSettings = {
+			filter: {
+				expression: expression
+			}
+		};
+
+		updateFilters(requestSettings);
+	};
+	
+	this.setViewSettings = function(requestSettings) {
+		updateFilters(requestSettings);
+	};
+
+	this.refresh = function () {
+		updateFilters();
+	};
+
+	this.reset = function () {
+		viewSettings = angular.copy(defaultViewSettings);
+		updateFilters();
+	};
+}
+
+angular.module("lightGridDataProviders").provider("lgServerDataProviderFactory", function () {
+
+	var self = this;
+
+	this.defaultViewSettings = {
+		orderBy: null,
+		limitTo: null,
+		filter: null
+	};
+	
+	this.debounceTime = 150;
+
+	this.$get = function($http, $timeout) {
+		return {
+			create: function(resource) {
+				return new ServerDataProvider(resource, $http, $timeout, self.defaultViewSettings, self.debounceTime);
+			}
+		};
+	};
+});
---
+++ test/unit/lightGridDataProviders/serverDataProviderTests.js
@@ -0,0 +1,262 @@
+/// <reference path="../../../typings/jasmine/jasmine.d.ts"/>
+
+describe("Server data provider", function() {
+	"use strict";
+
+	var $timeout;
+	var $httpBackend;
+	var providerFactory;
+	var dataProvider;
+	var testResourceUrl = "http://site.com/my-resource";
+	var responseStub = {
+		data: [{id: 1}],
+		totalResults: 14
+	};
+	
+	beforeEach(function () { 
+		module("lightGridDataProviders");
+	});
+
+	beforeEach(inject(function (_$timeout_, _$httpBackend_, lgServerDataProviderFactory) {
+		$timeout = _$timeout_;
+		$httpBackend = _$httpBackend_;
+		providerFactory = lgServerDataProviderFactory;
+		dataProvider = providerFactory.create(testResourceUrl);
+	}));
+	
+	afterEach(function() {
+		$httpBackend.verifyNoOutstandingExpectation();
+		$httpBackend.verifyNoOutstandingRequest();
+	});
+		
+	describe("when viewSettings are set to their defaults", function() {
+		it("should not have search, order or paging options applied", function() {
+			var viewSettings = dataProvider.getCurrentViewSettings();
+			
+			expect(viewSettings.filter).toBe(null);
+			expect(viewSettings.orderBy).toBe(null);
+			expect(viewSettings.limitTo).toBe(null);
+		});
+		
+		it("should issue a GET request to the specified URL without adding query string", function() {
+			$httpBackend.expectGET(testResourceUrl).respond(responseStub);
+			dataProvider.refresh();
+			$timeout.flush();
+			$httpBackend.flush();
+		});
+		
+		it("should make the returned data available via the getGridModel method", function() {
+			$httpBackend.whenGET(testResourceUrl).respond(responseStub);
+			dataProvider.refresh();
+			$timeout.flush();
+			$httpBackend.flush();
+			
+			expect(dataProvider.getGridModel()).toEqual(responseStub.data);
+		});
+		
+		it("should make the returned record count available via the getModelItemCount method", function() {
+			$httpBackend.whenGET(testResourceUrl).respond(responseStub);
+			dataProvider.refresh();
+			$timeout.flush();
+			$httpBackend.flush();
+			
+			expect(dataProvider.getModelItemCount()).toEqual(responseStub.totalResults);
+		});
+	});
+	
+	describe("#limitTo", function() {
+		describe("with limit parameter only", function() {
+			it("should issue a GET request with a proper query string", function() {
+				$httpBackend.expectGET(testResourceUrl + "?limit=15").respond(responseStub);
+				dataProvider.limitTo(15);
+				$timeout.flush();
+				$httpBackend.flush();
+			});
+			
+			it("should persist the settings after the response is received", function() {
+				$httpBackend.whenGET(testResourceUrl + "?limit=15").respond(responseStub);
+				dataProvider.limitTo(15);
+				var viewSettings = dataProvider.getCurrentViewSettings();
+				expect(viewSettings.limitTo).toBe(null);
+				
+				$timeout.flush();
+				$httpBackend.flush();
+				
+				viewSettings = dataProvider.getCurrentViewSettings();
+				expect(viewSettings.limitTo.limit).toBe(15);
+				expect(viewSettings.limitTo.begin).toBe(0);
+			});
+		});
+		
+		describe("with both limit and begin parameters", function() {
+			it("should issue a GET request with a proper query string", function() {
+				$httpBackend.expectGET(testResourceUrl + "?limit=15&begin=20").respond(responseStub);
+				dataProvider.limitTo(15, 20);
+				$timeout.flush();
+				$httpBackend.flush();
+			});
+			
+			it("should persist the settings after the response is received", function() {
+				$httpBackend.whenGET(testResourceUrl + "?limit=15&begin=20").respond(responseStub);
+				dataProvider.limitTo(15, 20);
+				var viewSettings = dataProvider.getCurrentViewSettings();
+				expect(viewSettings.limitTo).toBe(null);
+				
+				$timeout.flush();
+				$httpBackend.flush();
+				
+				viewSettings = dataProvider.getCurrentViewSettings();
+				expect(viewSettings.limitTo.limit).toBe(15);
+				expect(viewSettings.limitTo.begin).toBe(20);
+			});
+		});
+		
+		describe("with limit set to 0 and begin set to more than 0", function() {
+			it("should issue a GET request with a proper query string", function() {
+				$httpBackend.expectGET(testResourceUrl + "?begin=20").respond(responseStub);
+				dataProvider.limitTo(0, 20);
+				$timeout.flush();
+				$httpBackend.flush();
+			});
+		});
+	});
+	
+	describe("#orderBy", function() {
+		describe("with expression parameter only", function() {
+			it("should issue a GET request with a proper query string", function() {
+				$httpBackend.expectGET(testResourceUrl + "?orderBy=foo").respond(responseStub);
+				dataProvider.orderBy("foo");
+				$timeout.flush();
+				$httpBackend.flush();
+			});
+			
+			it("should persist the settings after the response is received", function() {
+				$httpBackend.whenGET(testResourceUrl + "?orderBy=foo").respond(responseStub);
+				dataProvider.orderBy("foo");
+				var viewSettings = dataProvider.getCurrentViewSettings();
+				expect(viewSettings.orderBy).toBe(null);
+				
+				$timeout.flush();
+				$httpBackend.flush();
+				
+				viewSettings = dataProvider.getCurrentViewSettings();
+				expect(viewSettings.orderBy.expression).toBe("foo");
+				expect(viewSettings.orderBy.reverse).toBe(false);
+			});
+		});
+		
+		describe("with both limit and begin parameters", function() {
+			it("should issue a GET request with a proper query string", function() {
+				$httpBackend.expectGET(testResourceUrl + "?orderBy=foo&reverse=true").respond(responseStub);
+				dataProvider.orderBy("foo", true);
+				$timeout.flush();
+				$httpBackend.flush();
+			});
+			
+			it("should persist the settings after the response is received", function() {
+				$httpBackend.whenGET(testResourceUrl + "?orderBy=foo&reverse=true").respond(responseStub);
+				dataProvider.orderBy("foo", true);
+				var viewSettings = dataProvider.getCurrentViewSettings();
+				expect(viewSettings.orderBy).toBe(null);
+				
+				$timeout.flush();
+				$httpBackend.flush();
+				
+				viewSettings = dataProvider.getCurrentViewSettings();
+				expect(viewSettings.orderBy.expression).toBe("foo");
+				expect(viewSettings.orderBy.reverse).toBe(true);
+			});
+		});
+	});
+	
+	describe("#filter", function() {
+		describe("with expression parameter", function() {
+			it("should issue a GET request with a proper query string", function() {
+				$httpBackend.expectGET(testResourceUrl + "?search=foo").respond(responseStub);
+				dataProvider.filter("foo");
+				$timeout.flush();
+				$httpBackend.flush();
+			});
+			
+			it("should persist the settings after the response is received", function() {
+				$httpBackend.whenGET(testResourceUrl + "?search=foo").respond(responseStub);
+				dataProvider.filter("foo");
+				var viewSettings = dataProvider.getCurrentViewSettings();
+				expect(viewSettings.filter).toBe(null);
+				
+				$timeout.flush();
+				$httpBackend.flush();
+				
+				viewSettings = dataProvider.getCurrentViewSettings();
+				expect(viewSettings.filter.expression).toBe("foo");
+			});
+		});
+	});
+	
+	describe("#setViewSettings", function() {
+		describe("with custom settings object", function() {
+			it("should issue a GET request with a proper query string", function() {
+				$httpBackend.expectGET(testResourceUrl + "?limit=10&begin=20&orderBy=id&reverse=true&search=foo").respond(responseStub);
+				
+				var customSettings = {
+					orderBy: {
+						expression: "id",
+						reverse: true
+					},
+					limitTo: {
+						limit: 10,
+						begin: 20	
+					},
+					filter: {
+						expression: "foo"
+					}
+				};
+				
+				dataProvider.setViewSettings(customSettings);
+				$timeout.flush();
+				$httpBackend.flush();
+			});
+		});
+	});
+	
+	describe("#reset", function() {
+		it("should reset view settings to their defaults", function() {
+			$httpBackend.whenGET(testResourceUrl + "?search=foo").respond(responseStub);
+			
+			dataProvider.filter("foo");
+			$timeout.flush();
+			$httpBackend.flush();
+			
+			$httpBackend.expectGET(testResourceUrl).respond(responseStub);
+			
+			dataProvider.reset();
+			$timeout.flush();
+			$httpBackend.flush();
+			
+			var viewSettings = dataProvider.getCurrentViewSettings();
+			expect(viewSettings.filter).toBe(null);
+			expect(viewSettings.orderBy).toBe(null);
+			expect(viewSettings.limitTo).toBe(null);
+		});
+	});
+	
+	describe("subsequent calls", function() {
+		it("should add up instead of overwriting view settings", function() {
+			$httpBackend.whenGET(testResourceUrl + "?search=foo").respond(responseStub);
+			
+			dataProvider.filter("foo");
+			$timeout.flush();
+			$httpBackend.flush();
+			
+			$httpBackend.expectGET(testResourceUrl + "?orderBy=id&search=foo").respond(responseStub);
+			
+			dataProvider.orderBy("id");
+			$timeout.flush();
+			$httpBackend.flush();
+			
+			var viewSettings = dataProvider.getCurrentViewSettings();
+			expect(viewSettings.filter.expression).toBe("foo");
+			expect(viewSettings.orderBy.expression).toBe("id");
+		});
+	});
+});
