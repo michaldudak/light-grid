64ef39c37b583ef8ab2638a0a86e7be915351b15
Michał Dudak <michal.dudak@gmail.com>
2015-06-29 21:08:01 +0200

    Typo corrected

--- src/lightGridControls/toggleExpandedRow.js
+++ src/lightGridControls/toggleExpandedRow.js
@@ -1,6 +1,6 @@
 ﻿/**
  * Opens or closes the expanded view of the row.
- * This can be only used as an attribute . It's value specifies the name of the template
+ * This can be only used as an attribute. Its value specifies the name of the template
  * used as an expanded row content.
  */
 angular.module("lightGridControls").directive("lgToggleExpandedRow", function () {
