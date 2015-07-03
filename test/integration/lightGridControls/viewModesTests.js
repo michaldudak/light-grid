4225da4403365a9db23084d72fbba3e0a7834c13
Michał Dudak <michal.dudak@gmail.com>
2015-07-03 12:21:55 +0200

    View modes tests refactoring

--- test/integration/lightGridControls/viewModesTests.js
+++ test/integration/lightGridControls/viewModesTests.js
@@ -36,84 +36,98 @@ describe("Light Grid: View modes tests", function () {
 		];
 	}));
 
-	it("should initially render the default view", function () {
-		var element = $compile(gridMarkup)($rootScope);
-		$rootScope.$digest();
-		var cell = element.find("td:first");
-
-		expect(cell.text()).toBe("Default view");
-		expect(element.html()).not.toMatch("Alternate view");
+	describe("when initial-view attribute is not set", function () {
+		it("should initially render the default view", function () {
+			var element = $compile(gridMarkup)($rootScope);
+			$rootScope.$digest();
+			var cell = element.find("td:first");
+	
+			expect(cell.text()).toBe("Default view");
+			expect(element.html()).not.toMatch("Alternate view");
+		});
 	});
 
-	it("should initially render the view specified in initial-view attribute", function () {
-		var element = $compile(gridWithAlternateViewMarkup)($rootScope);
-		$rootScope.$digest();
-		var cell = element.find("td:first");
-
-		expect(cell.text()).toBe("Alternate view");
-		expect(element.html()).not.toMatch("Default view");
+	describe("when initial-view attribute is set", function () {
+		it("should initially render the view specified in initial-view attribute", function () {
+			var element = $compile(gridWithAlternateViewMarkup)($rootScope);
+			$rootScope.$digest();
+			var cell = element.find("td:first");
+	
+			expect(cell.text()).toBe("Alternate view");
+			expect(element.html()).not.toMatch("Default view");
+		});
 	});
-
-	it("should change the view of a row when switchView is called", function () {
-		var element = $compile(gridMarkup)($rootScope);
-		$rootScope.$digest();
-
-		var firstRow = element.find("tbody tr").eq(0);
-		var secondRow = element.find("tbody tr").eq(1);
-
-		var rowController = firstRow.scope().rowController;
-		rowController.switchView("alternate");
-		$rootScope.$digest();
-
-		expect(firstRow.find("td").text()).toBe("Alternate view");
-		expect(secondRow.find("td").text()).toBe("Default view");
-	});
-
-	it("should change the view of all rows when grid's switchView is called", function () {
-		var element = $compile(gridMarkup)($rootScope);
-		$rootScope.$digest();
-
-		var firstRow = element.find("tbody tr").eq(0);
-		var secondRow = element.find("tbody tr").eq(1);
-
-		var gridController = firstRow.scope().gridController;
-		gridController.switchView("alternate");
-		$rootScope.$digest();
-
-		expect(firstRow.find("td").text()).toBe("Alternate view");
-		expect(secondRow.find("td").text()).toBe("Alternate view");
-	});
-
-	it("should show the default view when switchView is called with unknown view name", function () {
-		var element = $compile(gridWithAlternateViewMarkup)($rootScope);
-		$rootScope.$digest();
-
-		var firstRow = element.find("tbody tr").eq(0);
-		var secondRow = element.find("tbody tr").eq(1);
-
-		var rowController = firstRow.scope().rowController;
-		rowController.switchView("unknown");
-		$rootScope.$digest();
-
-		expect(firstRow.find("td").text()).toBe("Default view");
-		expect(secondRow.find("td").text()).toBe("Alternate view");
-	});
-
-	it("should show the proper view when it's declared as multiview", function () {
-		var element = $compile(gridWithMultiviewsMarkup)($rootScope);
-		$rootScope.$digest();
-
-		var firstRow = element.find("tbody tr").eq(0);
-		var secondRow = element.find("tbody tr").eq(1);
-
-		var firstRowController = firstRow.scope().rowController;
-		var secondRowController = secondRow.scope().rowController;
-
-		firstRowController.switchView("alternate1");
-		secondRowController.switchView("alternate2");
-		$rootScope.$digest();
-
-		expect(firstRow.find("td").text()).toBe("Alternate view");
-		expect(secondRow.find("td").text()).toBe("Alternate view");
+	
+	describe("when switchView is called", function () {
+		describe("on a row, with a known view name", function () {
+			it("should change the view of a row", function () {
+				var element = $compile(gridMarkup)($rootScope);
+				$rootScope.$digest();
+		
+				var firstRow = element.find("tbody tr").eq(0);
+				var secondRow = element.find("tbody tr").eq(1);
+		
+				var rowController = firstRow.scope().rowController;
+				rowController.switchView("alternate");
+				$rootScope.$digest();
+		
+				expect(firstRow.find("td").text()).toBe("Alternate view");
+				expect(secondRow.find("td").text()).toBe("Default view");
+			});
+		});
+		
+		describe("on a row, with unknown view name", function () {
+			it("should show the default view", function () {
+				var element = $compile(gridWithAlternateViewMarkup)($rootScope);
+				$rootScope.$digest();
+		
+				var firstRow = element.find("tbody tr").eq(0);
+				var secondRow = element.find("tbody tr").eq(1);
+		
+				var rowController = firstRow.scope().rowController;
+				rowController.switchView("unknown");
+				$rootScope.$digest();
+		
+				expect(firstRow.find("td").text()).toBe("Default view");
+				expect(secondRow.find("td").text()).toBe("Alternate view");
+			});
+		});
+		
+		describe("on a grid", function () {
+			it("should change the view of all rows when grid's switchView is called", function () {
+				var element = $compile(gridMarkup)($rootScope);
+				$rootScope.$digest();
+		
+				var firstRow = element.find("tbody tr").eq(0);
+				var secondRow = element.find("tbody tr").eq(1);
+		
+				var gridController = firstRow.scope().gridController;
+				gridController.switchView("alternate");
+				$rootScope.$digest();
+		
+				expect(firstRow.find("td").text()).toBe("Alternate view");
+				expect(secondRow.find("td").text()).toBe("Alternate view");
+			});
+		});
+		
+		describe("when a view is declared as multiview", function () {
+			it("should show the proper view", function () {
+				var element = $compile(gridWithMultiviewsMarkup)($rootScope);
+				$rootScope.$digest();
+		
+				var firstRow = element.find("tbody tr").eq(0);
+				var secondRow = element.find("tbody tr").eq(1);
+		
+				var firstRowController = firstRow.scope().rowController;
+				var secondRowController = secondRow.scope().rowController;
+		
+				firstRowController.switchView("alternate1");
+				secondRowController.switchView("alternate2");
+				$rootScope.$digest();
+		
+				expect(firstRow.find("td").text()).toBe("Alternate view");
+				expect(secondRow.find("td").text()).toBe("Alternate view");
+			});
+		});
 	});
 });
